8c8dfc4a16375ed54586beca3e554ae6ce0cf2b6
KillianVendewinkele <killian.vendewinkele@edu.esiee-it.fr>
2023-05-03 12:39:19 +0200

    need Dishes

--- src/main/java/com/example/javaresto/OrderController.java
+++ src/main/java/com/example/javaresto/OrderController.java
@@ -1,14 +1,22 @@
 package com.example.javaresto;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-public class OrderController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class OrderController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView listViewOrder;
+
+    
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/main/java/com/example/javaresto/classes/Order.java
+++ src/main/java/com/example/javaresto/classes/Order.java
@@ -1,4 +1,53 @@
 package com.example.javaresto.classes;
 
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Order {
+    private List<Dish> dishes;
+    private String customer;
+    private String status;
+    private int netPrice;
+    private int rawPrice;
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getNetPrice() {
+        return netPrice;
+    }
+
+    public int getRawPrice() {
+        return rawPrice;
+    }
+
+    public Order(List<Dish> dishes, String customer, String status, int netPrice, int rawPrice) {
+        this.dishes = dishes;
+        this.customer = customer;
+        this.status = status;
+        this.netPrice = netPrice;
+        this.rawPrice = rawPrice;
+    }
+
+    public void CreateListOrder() {
+
+
+        
+        /*List<Label> labels = list
+             .stream()
+             .map( Label::new )
+             .collect(Collectors.toList());*/
+    }
 }
