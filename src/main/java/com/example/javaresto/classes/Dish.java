f7452e90799d7b8681070def486493d89db2f0a8
mathias-smj <91052046+mathias-smj@users.noreply.github.com>
2023-05-03 11:40:18 +0200

    bug
    null

--- src/main/java/com/example/javaresto/classes/Dish.java
+++ src/main/java/com/example/javaresto/classes/Dish.java
@@ -1,5 +1,16 @@
 package com.example.javaresto.classes;
 
+import javafx.scene.image.Image;
+import java.util.List;
+
 public class Dish {
-    
+
+    private String name;
+    private String description;
+    private double netPrice;
+
+    private double grossPrice;
+
+    private List<Dish> dishList;
+    private Image image;
 }
