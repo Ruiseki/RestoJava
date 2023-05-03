c521b3bb14ddf3f5f8bb08d18dfbfa86de4754fa
mathias-smj <91052046+mathias-smj@users.noreply.github.com>
2023-05-03 11:47:32 +0200

    Class Ingredient
    add class and properties

--- src/main/java/com/example/javaresto/classes/Dish.java
+++ src/main/java/com/example/javaresto/classes/Dish.java
@@ -1,5 +1,6 @@
 package com.example.javaresto.classes;
 
+
 import javafx.scene.image.Image;
 import java.util.List;
 
@@ -13,4 +14,49 @@ public class Dish {
 
     private List<Dish> dishList;
     private Image image;
+
+    public Dish(String name, String description, double netPrice, double grossPrice, List<Dish> dishList, Image image){
+
+        this.name = name;
+        this.description = description;
+        this.netPrice = netPrice;
+        this.grossPrice = grossPrice;
+        this.dishList = dishList;
+        this.image = image;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getNetPrice() {
+        return netPrice;
+    }
+
+    public double getGrossPrice() {
+        return grossPrice;
+    }
+
+    public List<Dish> getDishList() {
+        return dishList;
+    }
+
+    public void setDishList(List<Dish> dishList) {
+        this.dishList = dishList;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
