14c9962ea79fdae42707fd108ebe447b255b1fc5
mathias-smj <91052046+mathias-smj@users.noreply.github.com>
2023-05-04 11:43:53 +0200

    Add different functions to create a dish, add a dish and display the dish and the details of the dish

--- src/main/java/com/example/javaresto/HelloApplication.java
+++ src/main/java/com/example/javaresto/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.javaresto;
 
+import com.example.javaresto.classes.Dish;
 import com.example.javaresto.classes.Ingredient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,7 +23,21 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
 
+        /*Image pizzaImage = new Image("/Users/smajicmathias/Desktop/JavaResto/src/main/resources/com/example/javaresto/image/pizzaMargherita.jpg");
+        Image saladImage = new Image("/Users/smajicmathias/Desktop/JavaResto/src/main/resources/com/example/javaresto/image/saladCesar.jpg");
+        Image pastaImage = new Image("/Users/smajicmathias/Desktop/JavaResto/src/main/resources/com/example/javaresto/image/pastaBolo.jpg");*/
+
+        Dish.addDish(Dish.createDish("Spaghetti bolognese", "Pasta with bolognese sauce", 8.50, 10.00, null));
+        Dish.addDish(Dish.createDish("Pizza margherita", "Tomate, mozzarella, basilic", 9.00, 11.00, null));
+        Dish.addDish(Dish.createDish("Caesar salad", "Green salad, chicken, parmesan, croutons", 7.50, 9.00, null));
+
+        // Afficher la liste de plats
+        Dish.displayMenu();
+
+        // Afficher les détails d'un plat spécifique
+        Dish.displayDishDetails("Caesar salad");
+
+        launch();
     }
 }
